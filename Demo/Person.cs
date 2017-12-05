6c2f8b86123b4fb06fe0a22a0d461542e8d88306
Milan Martiniak <mino.martiniak@gmail.com>
2017-12-05 11:58:40 +0100

    Zalozenie prezentacie a vytvorenie zakladu dema

---
+++ Demo/Person.cs
@@ -0,0 +1,11 @@
+namespace MMLib.Demo.SOLIDPrinciples
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Age { get; set; }
+        public int Division { get; set; }
+
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using Kros.KORM;
+using Kros.KORM.Metadata.Attribute;
+using System.Linq;
+
+namespace MMLib.Demo.SOLIDPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var connectionString = args[0];
+                var mailTo = args[1];
+                var division = int.Parse(args[2]);
+
+                using(var database = new Database(connectionString, "System.Data.SqlClient"))
+                {
+                    var sb = new StringBuilder();
+                    var people = database
+                        .Query<Person>()
+                        .Where(p => p.Division == division);
+
+                    sb.AppendLine(@"<html>
+                                    <body>
+                                        <table>");
+                    sb.AppendLine(@"<tr>
+                                    <th>Firstname</th>
+                                    <th>Lastname</th>
+                                    <th>Age</th>
+                                </tr>");
+
+                    foreach (var person in people)
+                    {
+                        sb.Append($@"<tr>
+                                        <td>{person.FirstName}</td>
+                                        <td>{person.LastName}</td>
+                                        <td>{person.Age}</td>
+                                    </tr>");
+                    }
+
+                    sb.AppendLine("</table></body></html>");
+
+                    using(var client = new SmtpClient("myServerHost"))
+                    using(var mailMessage = new MailMessage())
+                    {
+                        // ..
+                        mailMessage.To.Add(mailTo);
+                        mailMessage.Body = sb.ToString();
+
+                        client.Send(mailMessage);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(@"C:\log.log", ex.ToString());
+                throw;
+            }
+        }
+    }
+}
