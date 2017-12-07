cc42623f83b831f83a1f7d0286d98619896197df
Miňo Martiniak <mino.martiniak@gmail.com>
2017-12-07 20:55:18 +0100

    Update XlsPeopleRepository.cs

--- Demo/Models/XlsPeopleRepository.cs
+++ Demo/Models/XlsPeopleRepository.cs
@@ -5,9 +5,9 @@ namespace MMLib.Demo.SOLIDPrinciples
     public class XlsPeopleRepository : PeopleRepository
     {
         public override void Add(Person person) =>
-            throw new InvalidOperationException("Repository is ReadOnly");
+            throw new NotImplementedException("Repository is ReadOnly");
 
         public override void CommitChanges() =>
-            throw new InvalidOperationException("Repository is ReadOnly");
+            throw new NotImplementedException("Repository is ReadOnly");
     }
 }
