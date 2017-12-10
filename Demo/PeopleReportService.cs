76ab256d94dc37ff68389859e486f8c2717d941b
Miňo Martiniak <mino.martiniak@gmail.com>
2017-12-10 21:57:42 +0100

    Update PeopleReportService.cs

--- Demo/PeopleReportService.cs
+++ Demo/PeopleReportService.cs
@@ -2,17 +2,23 @@ using Kros.Utils;
 
 namespace MMLib.Demo.SOLIDPrinciples
 {
-    public class PeopleReportService: IPeopleReportService
+    public class PeopleReportService : IPeopleReportService
     {
         private IReadOnlyPeopleRepository _peopleRepository;
-        private IMailServer _mailServer = new MailServer();
-        private IPeopleReportDataFormatter _reportDataFormatter = new PeopleReportDataFormatter();
+        private IMailServer _mailServer;
+        private IPeopleReportDataFormatter _reportDataFormatter;
 
-        public PeopleReportService(IReadOnlyPeopleRepository peopleRepository)
+        public PeopleReportService(IReadOnlyPeopleRepository peopleRepository,
+            IMailServer mailServer,
+            IPeopleReportDataFormatter peopleReportDataFormatter)
         {
             Check.NotNull(peopleRepository, nameof(peopleRepository));
+            Check.NotNull(mailServer, nameof(mailServer));
+            Check.NotNull(peopleReportDataFormatter, nameof(peopleReportDataFormatter));
 
             _peopleRepository = peopleRepository;
+            _reportDataFormatter = peopleReportDataFormatter;
+            _mailServer = mailServer;
         }
 
         public void SendReport(int division, string mailTo)
